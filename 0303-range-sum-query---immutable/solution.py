770428c66cd5fe6913b3ea0fcd9272acf7c9da07
Satyasrigudala <satyasrigudala64@gmail.com>
2025-09-08 04:22:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.9 MB (99.98%)

---
+++ 0303-range-sum-query---immutable/solution.py
@@ -0,0 +1,9 @@
+class NumArray:
+    def __init__(self, nums: List[int]):
+        # Build prefix sum array
+        self.prefix = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            self.prefix[i+1] = self.prefix[i] + nums[i]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right+1] - self.prefix[left]
