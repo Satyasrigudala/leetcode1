2d96ec4f9bf5fa980a16da8a85e4734a9b2f3f52
Satyasrigudala <satyasrigudala64@gmail.com>
2025-10-08 17:39:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ 0119-pascals-triangle-ii/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> list[int]:
+        row = [1]
+        
+        for i in range(1, rowIndex + 1):
+            # Build next row from the previous one
+            new_row = [1]
+            for j in range(1, i):
+                new_row.append(row[j - 1] + row[j])
+            new_row.append(1)
+            row = new_row
+        
+        return row
+
