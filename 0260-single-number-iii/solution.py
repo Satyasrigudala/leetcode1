e754bdd68c21335cca02f5ababba3fd8a4e1ca62
Satyasrigudala <satyasrigudala64@gmail.com>
2026-06-03 00:26:43 +0000

    Sync LeetCode submission Runtime - 3914 ms (5.01%), Memory - 20.8 MB (87.60%)

---
+++ 0260-single-number-iii/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in nums:
+            if nums.count(i) == 1:
+                ans.append(i)
+        return ans
+
+
+        
