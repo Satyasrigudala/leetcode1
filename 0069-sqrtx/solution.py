ac68e2332edb66fa61cc3f38a0b8b630fa2b9ebe
Satyasrigudala <satyasrigudala64@gmail.com>
2026-06-07 12:08:02 +0000

    Sync LeetCode submission Runtime - 1176 ms (13.11%), Memory - 19.1 MB (87.80%)

--- 0069-sqrtx/solution.py
+++ 0069-sqrtx/solution.py
@@ -1,18 +1,9 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x < 2:  # Handle 0 and 1 quickly
-            return x
-
-        l, r = 1, x
-        while l <= r:
-            m = (l + r) // 2
-            msq = m * m
-            if msq == x:
-                return m
-            elif msq < x:
-                l = m + 1
+        ans = 0
+        for i in range(x+ 1):
+            if i * i <= x:
+                ans = i
             else:
-                r = m - 1
-
-        return r  # r will be the floor(sqrt(x))
-
+                break
+        return ans       
