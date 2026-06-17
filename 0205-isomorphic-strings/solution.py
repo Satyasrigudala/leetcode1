ca9c4f0eb5a5b73b40945e3d60b554c93d7dfeda
Satyasrigudala <satyasrigudala64@gmail.com>
2026-06-17 00:32:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (82.17%)

---
+++ 0205-isomorphic-strings/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+       if len(set(zip(s, t))) == len(set(s)) == len(set(t)):
+           return True
+       else:
+           return False
+
+            
+            
+        
