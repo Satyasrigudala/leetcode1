e4e61e19edace0e2a8c552000234f5f821553474
Satyasrigudala <satyasrigudala64@gmail.com>
2026-06-21 16:50:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (15.66%)

---
+++ 0551-student-attendance-record-i/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        for i in s:
+            if s.count('A')<2 and 'LLL'not in s:
+                return True
+            else:
+                return False   
+        
